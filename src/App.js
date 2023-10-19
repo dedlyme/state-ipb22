a53daf351ee5353e1bc981487c1f90ff0fe08f5e
Skolnieks <skolnieks.pjvt@pjvt.local>
2023-10-19 09:47:43 +0300

    darbs

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  const [ratio, setRatio] = useState(0);
+  const[ratioChange, setRatioChange] = useState(1);
+  function PlusRatio() {
+    setRatio(ratio + 1);
+    console.log("RATIO +1");
+    console.log("ratio ir " + ratio);
+  }
+  const [nratio, setNratio] = useState(0);
+  function NegativeRatio() {
+    setNratio(nratio - 1);
+    console.log("NRATIO - 1");
+    console.log("nratio ir" + nratio);
+  }
+    function handleRatioChange(event) {
+      setRatioChange(event.target.value);
+    }
+
+  
+  return ( 
+  <div className="App">
+    <input type="number" value={ratioChange} onChange={handleRatioChange}></input>
+  <button onClick={PlusRatio}>RATIO +1{ratioChange}</button>
+  <button onClick={NegativeRatio}>RATIO -1{ratioChange}</button>
+    <h1>{ratio}</h1>
+  </div>
   );
 }
 
